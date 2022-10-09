d5e570b90ddab245ac0b520a6390e76c565e5a5a
baronmdw <58744855+baronmdw@users.noreply.github.com>
2022-10-09 20:53:43 +0200

    page layout adaptive, dynamic navbar with toggle

---
+++ js/app.js
@@ -0,0 +1,35 @@
+function setupNavbar() {
+    // After initiating the DOM this function collects all card-headings and adds a navbar element for each
+    let t1 = performance.now();
+    const chapters = document.querySelectorAll(".card");
+    const navbarWrapper = document.querySelector(".navbar");
+    // TODO: Performance optimization
+    for (const chapter of chapters) {
+        let addingElement = document.createElement("div");
+        addingElement.classList.add("navbar-element");
+        addingElement.innerText = chapter.firstElementChild.textContent
+        navbarWrapper.appendChild(addingElement);
+        addingElement.addEventListener('click',navbarElementClicked);
+    };
+    let t2 = performance.now();
+    console.log(t2-t1);
+}
+
+function navbarElementClicked(event) {
+    // This function checks which element was clicked, deactivates all other navbar elements and toggles the one that was clicked
+    const t1 = performance.now()
+    const clickedElement = event.target;
+    const parentElement = clickedElement.parentElement;
+    const wasClicked = clickedElement.classList.contains("highlight");
+    // TODO: Performance optimization
+    for (const child of parentElement.children) {
+        child.classList.remove("highlight")
+    };
+    if (wasClicked === false) {
+        clickedElement.classList.add("highlight");
+    };
+    let t2 = performance.now();
+    console.log(t2-t1);
+}
+
+setupNavbar();
